b5088edcd46d18e517f07a230f8807d061712968
Dale Black <djblack@uci.edu>
2023-07-21 11:27:54 -0700

    add `taylor_series_exp` and tests

---
+++ src/intro_math_methods.jl
@@ -0,0 +1,39 @@
+module intro_math_methods
+
+using Symbolics
+
+export taylor_series_exp
+
+
+"""
+    taylor_series_exp(x::Num, n::Int)
+
+Compute the Taylor series expansion of `exp(x)` up to the `n`th term about the origin (0).
+
+#### Arguments
+- `x::Num` : The point around which to expand the function.
+- `n::Int` : The number of terms in the Taylor series expansion.
+
+#### Returns
+- `::Number` : The calculated value of the Taylor series expansion of `exp(x)` up to the `n`th term.
+
+#### Example
+```julia
+using Symbolics
+
+@variables x
+taylor_series_exp(x, 5)  
+
+# Returns
+julia > 1 + x + x^2/2 + x^3/6 + x^4/24 + x^5/120
+```
+"""
+function taylor_series_exp(x::Num, n::Int)
+    sum_expr = 0
+    for i in 0:n
+        sum_expr += x^i // factorial(i)
+    end
+    return sum_expr
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
-using intro-math-methods
+using intro_math_methods
 using Test
+using Symbolics
 
 @testset "intro-math-methods.jl" begin
-    # Write your tests here.
+    @variables x
+    @testset "Taylor Series" begin
+        sym_exp = taylor_series_exp(x, 10)
+        @test isapprox(exp(1), Float64(substitute(sym_exp, x => 1).val); atol = 1e-2)
+        @test isapprox(exp(2), Float64(substitute(sym_exp, x => 2).val); atol = 1e-2)
+    end
 end
